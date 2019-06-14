102a4653696b8d6213c0c8af9a6d6b60469d8c4d
dxu <dean.xu@asml.com>
2019-06-14 14:04:23 +0800

    [xecho] add error test

--- xecho/util.go
+++ xecho/util.go
@@ -1,36 +1,9 @@
 package xecho
 
-import (
-	"github.com/labstack/echo/v4"
-)
-
 type J map[string]interface{}
 
 func M(msg string) J {
 	return J{
 		"message": msg,
-	}
-}
-
-func MustBind(c echo.Context, param interface{}) {
-	if err := c.Bind(param); err != nil {
-		panic(BreakError{Actual: err})
-	}
-}
-
-func MustValidate(c echo.Context, param interface{}) {
-	if err := c.Validate(param); err != nil {
-		panic(BreakError{Actual: err})
-	}
-}
-
-func MustBindAndValidate(c echo.Context, param interface{}) {
-	MustBind(c, param)
-	MustValidate(c, param)
-}
-
-func MustNoError(err error) {
-	if err != nil {
-		panic(BreakError{Actual: err})
 	}
 }
